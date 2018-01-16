708d009b3b17304bf7ae22b65c00019477dec413
Kensuke Hoshikawa <kensuke-hoshikawa@cookpad.com>
2018-01-16 12:54:56 +0900

    testing transaction

--- functions/src/__tests__/index.test.ts
+++ functions/src/__tests__/index.test.ts
@@ -1,5 +1,40 @@
+import * as admin from 'firebase-admin'
 import 'jest'
 
-it('adds 1 + 2 to equal 3 in TScript', () => {
-  expect(3).toBe(3)
+beforeAll(() => {
+  const serviceAccount = require('../../sandbox-329fc-firebase-adminsdk.json')
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+  })
+})
+
+it('order create', async () => {
+  const skuData = [...Array(3).keys()].map(a => {
+    return { price: 5000, stock: 1000 }
+  })
+  const skus = await Promise.all(skuData.map(data => {
+    return admin.firestore().collection('version/1/testsku').add(data)
+  }))
+
+  const testOrderColRef = admin.firestore().collection('version/1/testorder')
+  const testOrderColRef2 = admin.firestore().collection('version/1/testorder2')
+  const testOrders = [...Array(300).keys()].map(a => {
+    const testOrder: any = {}
+    testOrder.orderSKUs = skus
+    return testOrder
+  })
+  await Promise.all(testOrders.map(testOrder => {
+    return testOrderColRef.add(testOrder)
+  }))
+  await Promise.all(testOrders.map(testOrder => {
+    return testOrderColRef2.add(testOrder)
+  }))
+  // await Promise.all(testOrders.map(testOrder => {
+  //   return testOrderColRef.add(testOrder)
+  // }))
+  // await Promise.all(testOrders.map(testOrder => {
+  //   return testOrderColRef2.add(testOrder)
+  // }))
+
+  expect(true)
 })
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,45 @@
 import * as functions from 'firebase-functions'
+import * as admin from 'firebase-admin'
+
+admin.initializeApp(<admin.AppOptions>functions.config().firebase)
 
 export const helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!\n\n")
+  // response.send("Hello from Firebase!\n\n")
 })
+
+export const createTestOrder = functions.firestore.document(`/version/1/testorder/{testOrderID}`).onCreate(async event => {
+  const testOrderData = event.data.data()
+  const testOrderID = event.params!.testOrderID
+
+  const skus = <FirebaseFirestore.DocumentReference[]>testOrderData.orderSKUs
+  await decreaseStock(skus)
+
+  return undefined
+})
+
+export const createTestOrder2 = functions.firestore.document(`/version/1/testorder2/{testOrderID}`).onCreate(async event => {
+  const testOrderData = event.data.data()
+  const testOrderID = event.params!.testOrderID
+  console.log(testOrderID, 'stert')
+
+  const skus = <FirebaseFirestore.DocumentReference[]>testOrderData.orderSKUs
+  await decreaseStock(skus)
+
+  console.log(testOrderID, 'finish')
+  return undefined
+})
+
+const decreaseStock = async (skuRefs: FirebaseFirestore.DocumentReference[]) => {
+  for (const sku of skuRefs) {
+    await admin.firestore().runTransaction(transaction => {
+      return transaction.get(sku).then(skuDoc => {
+        const newStock = skuDoc.data().stock - 1
+        console.log(sku.id, newStock)
+        transaction.update(sku, { stock: newStock })
+      }).catch(e => {
+        console.error(sku.id, e)
+        throw 3
+      })
+    })
+  }
+}
