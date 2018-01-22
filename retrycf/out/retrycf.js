f4aeb54ce975e2693f4829cd25478857d4f7cc0f
Kensuke Hoshikawa <kensuke-hoshikawa@cookpad.com>
2018-01-22 11:43:51 +0900

    out

---
+++ retrycf/out/retrycf.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Status;
+(function (Status) {
+    Status[Status["none"] = 0] = "none";
+    Status[Status["success"] = 1] = "success";
+    Status[Status["failure"] = 2] = "failure";
+})(Status = exports.Status || (exports.Status = {}));
