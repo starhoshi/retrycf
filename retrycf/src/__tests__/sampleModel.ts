f4b4c5f7bdb68d332f1e17b1dc59f695c4248751
Kensuke Hoshikawa <kensuke-hoshikawa@cookpad.com>
2018-02-07 01:36:25 +0900

    fix type name

--- retrycf/src/__tests__/sampleModel.ts
+++ retrycf/src/__tests__/sampleModel.ts
@@ -2,5 +2,5 @@ import * as Retrycf from '../retrycf'
 import { Pring, property } from 'pring'
 
 export class SampleOrder extends Pring.Base implements Retrycf.HasNeoTask {
-  @property neoTask?: Retrycf.PNeoTask | undefined
+  @property neoTask?: Retrycf.NeoTask | undefined
 }
