f9428461566bfe830484cf5299837e9d8aa88ca5
Kensuke Hoshikawa <kensuke-hoshikawa@cookpad.com>
2018-02-09 15:20:25 +0900

    fix code comment

--- retrycf/src/retrycf.ts
+++ retrycf/src/retrycf.ts
@@ -49,7 +49,7 @@ export class Failure<T extends HasNeoTask> extends Pring.Base {
 
     if (querySnapshot.docs.length === 0) {
       const failure = new Failure()
-      // FIXME: ref を保存しようとすると Error: Cannot encode type ([object Object]) のエラーが出る
+      // FIXME: Error: Cannot encode type ([object Object])
       // failure.ref = documentSnapshot.ref
       failure.refPath = model.reference.path
       failure.neoTask = neoTask.rawValue()
@@ -57,7 +57,7 @@ export class Failure<T extends HasNeoTask> extends Pring.Base {
     } else {
       const failure = new Failure()
       failure.init(querySnapshot.docs[0])
-      // FIXME: ref を保存しようとすると Error: Cannot encode type ([object Object]) のエラーが出る
+      // FIXME: Error: Cannot encode type ([object Object])
       // failure.ref = documentSnapshot.ref
       failure.refPath = model.reference.path
       failure.neoTask = neoTask.rawValue()
@@ -129,7 +129,7 @@ export class NeoTask extends Pring.Base {
 
     neoTask.status = NeoTaskStatus.failure
     neoTask.retry.error.push(error.toString())
-    neoTask.retry.count += 1 // これをトリガーにして再実行する
+    neoTask.retry.count += 1 // retry trigger
 
     await model.reference.update({ neoTask: neoTask.rawValue() })
     await Failure.setFailure(model, neoTask)
@@ -191,17 +191,17 @@ export class NeoTask extends Pring.Base {
       return false
     }
 
-    // リトライカウントが3回以上だったら retry しない
+    // Not retry more than three times
     if (currentRetryCount >= NeoTask.MAX_RETRY_COUNT) {
       return false
     }
 
-    // リトライカウントがあるけど previous にはない
+    // Current neoTask.retry exists, but previous neoTask.retry does not exist
     if (!previousRetryCount) {
-      return true // 新しく retry が生成されたということになるので true
+      return true // true because a new retry has been generated
     }
 
-    // retry count が前回から変更されていたら retry する
+    // Returns true if the current retry count has increased from the previous retry count.
     return currentRetryCount > previousRetryCount
   }
 
@@ -231,162 +231,3 @@ export class NeoTask extends Pring.Base {
     return model
   }
 }
-
-// export interface INeoTask {
-//   status: NeoTaskStatus
-//   completed: { [id: string]: boolean }
-//   invalid?: { validationError: string, reason: string }
-//   retry?: { error: any[], count: number }
-//   fatal?: { step: string, error: string }
-// }
-
-// // TODO: Task を2回上書き保存した時に古いのが消える
-// export class NeoTask implements INeoTask {
-//   status: NeoTaskStatus = NeoTaskStatus.none
-//   completed: { [id: string]: boolean } = {}
-//   invalid?: { validationError: string, reason: string }
-//   retry?: { error: any[], count: number }
-//   fatal?: { step: string, error: string }
-
-//   static async markComplete(event: functions.Event<DeltaDocumentSnapshot>, transaction: FirebaseFirestore.Transaction, step: string) {
-//     const ref = firestore.doc(event.data.ref.path)
-//     console.log('retrycf ref', ref)
-//     return transaction.get(ref).then(tref => {
-//       if (NeoTask.isCompleted(event, step)) {
-//         throw new CompletedError(step)
-//       } else {
-//         const neoTask = new NeoTask(event.data)
-//         neoTask.completed[step] = true
-//         transaction.update(ref, { neoTask: neoTask.rawValue() })
-//       }
-//     })
-//   }
-
-//   static async clearComplete(event: functions.Event<DeltaDocumentSnapshot>) {
-//     const neoTask = new NeoTask(event.data)
-//     neoTask.completed = {}
-//     await event.data.ref.update({ neoTask: neoTask.rawValue() })
-//     event.data.data().neoTask = neoTask.rawValue()
-//   }
-
-//   static isCompleted(event: functions.Event<DeltaDocumentSnapshot>, step: string) {
-//     const neoTask = new NeoTask(event.data)
-//     return !!neoTask.completed[step]
-//   }
-
-//   static async setRetry(event: functions.Event<DeltaDocumentSnapshot>, step: string, error: any) {
-//     const neoTask = new NeoTask(event.data)
-
-//     if (!neoTask.retry) {
-//       neoTask.retry = { error: new Array(), count: 0 }
-//     }
-
-//     neoTask.status = NeoTaskStatus.failure
-//     neoTask.retry.error.push(error.toString())
-//     neoTask.retry.count += 1 // これをトリガーにして再実行する
-
-//     await event.data.ref.update({ neoTask: neoTask.rawValue() })
-//     // await Failure.setFailure(event.data, neoTask.rawValue())
-
-//     return neoTask
-//   }
-
-//   static async setInvalid(event: functions.Event<DeltaDocumentSnapshot>, error: ValidationError) {
-//     const neoTask = new NeoTask(event.data)
-
-//     neoTask.invalid = {
-//       validationError: error.validationErrorType,
-//       reason: error.reason
-//     }
-
-//     await event.data.ref.update({ neoTask: neoTask.rawValue() })
-
-//     return neoTask
-//   }
-
-//   static async setFatal(event: functions.Event<DeltaDocumentSnapshot>, step: string, error: any) {
-//     const neoTask = new NeoTask(event.data)
-
-//     neoTask.fatal = {
-//       step: step,
-//       error: error.toString()
-//     }
-
-//     console.log('fatal_error', event.data.ref.id)
-
-//     await event.data.ref.update({ neoTask: neoTask.rawValue() })
-//     // await Failure.setFailure(event.data, neoTask.rawValue())
-
-//     return neoTask
-//   }
-
-//   private static getRetryCount(data: DeltaDocumentSnapshot): number | undefined {
-//     const snapshotData = data.data()
-//     const currentNeoTask = snapshotData && <INeoTask>snapshotData.neoTask
-
-//     if (!(currentNeoTask && currentNeoTask.retry && currentNeoTask.retry.count)) {
-//       return undefined
-//     }
-
-//     return currentNeoTask.retry.count
-//   }
-
-//   private static MAX_RETRY_COUNT = 3
-//   static shouldRetry(data: DeltaDocumentSnapshot): boolean {
-//     const currentRetryCount = NeoTask.getRetryCount(data)
-//     const previousRetryCount = data.previous && NeoTask.getRetryCount(data.previous)
-
-//     if (!currentRetryCount) {
-//       return false
-//     }
-
-//     // リトライカウントが3回以上だったら retry しない
-//     if (currentRetryCount >= NeoTask.MAX_RETRY_COUNT) {
-//       return false
-//     }
-
-//     // リトライカウントがあるけど previous にはない
-//     if (!previousRetryCount) {
-//       return true // 新しく retry が生成されたということになるので true
-//     }
-
-//     // retry count が前回から変更されていたら retry する
-//     return currentRetryCount > previousRetryCount
-//   }
-
-//   static async setFatalIfRetryCountIsMax(event: functions.Event<DeltaDocumentSnapshot>) {
-//     const currentRetryCount = NeoTask.getRetryCount(event.data)
-//     const previousRetryCount = event.data.previous && NeoTask.getRetryCount(event.data.previous)
-
-//     if (currentRetryCount && previousRetryCount) {
-//       if (currentRetryCount >= NeoTask.MAX_RETRY_COUNT && currentRetryCount > previousRetryCount) {
-//         return NeoTask.setFatal(event, 'retry_failed', 'retry failed')
-//       }
-//     }
-//   }
-
-//   static async success(event: functions.Event<DeltaDocumentSnapshot>) {
-//     const neoTask: INeoTask = { status: NeoTaskStatus.success, completed: {} }
-//     await event.data.ref.update({ neoTask: neoTask })
-//     await Failure.deleteFailure(event.data.ref)
-//   }
-
-//   constructor(deltaDocumentSnapshot: DeltaDocumentSnapshot) {
-//     const neoTask = deltaDocumentSnapshot.data().neoTask
-//     if (neoTask) {
-//       if (neoTask.status) { this.status = neoTask.status }
-//       if (neoTask.completed) { this.completed = neoTask.completed }
-//       if (neoTask.invalid) { this.invalid = neoTask.invalid }
-//       if (neoTask.retry) { this.retry = neoTask.retry }
-//       if (neoTask.fatal) { this.fatal = neoTask.fatal }
-//     }
-//   }
-
-//   rawValue(): INeoTask {
-//     const neoTask: INeoTask = { status: this.status, completed: {} }
-//     if (this.invalid) { neoTask.invalid = this.invalid }
-//     if (this.retry) { neoTask.retry = this.retry }
-//     if (this.fatal) { neoTask.fatal = this.fatal }
-//     return neoTask
-//   }
-// }
